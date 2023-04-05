c06782a63792133c2bdee3a53074207d9a7be2a6
Alejandro Ouslan <unclearcoder@hotmail.com>
2023-04-05 17:16:35 +0200

    finished the photo prosses class

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from subprocess import Popen, PIPE
 from applescpt import scpt
+from camera import Camera
+import os
+import shutil
+
+path = '/Users/ouslan/Downloads/test'
+target = '/Users/ouslan/Downloads/target'
+os.chdir(path)
+c = Camera(path)
+iso = c.get_iso()
+date = c.get_date()
+os.mkdir(target, )
 
 
-script = scpt.format(args='1')
-p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-stdout, stderr = p.communicate(script)
+#for i in iso:
+#    shutil.move(i, target + '/' + str(iso[i]) + '_' + str(date[i]) + '.CR2')
+
+
+# script = scpt.format(args='')
+# p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+# stdout, stderr = p.communicate(script)
